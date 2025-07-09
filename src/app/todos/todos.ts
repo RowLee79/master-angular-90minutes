8945b01836437eaa71484dd1bb9c147bb3dce2cb
Rolly <Rolly@DESKTOP-QIJHOMP>
2025-07-09 16:35:21 +0800

    Initial commit

---
+++ src/app/todos/todos.ts
@@ -0,0 +1,44 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodoService } from '../services/todos';
+import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
+import { TodoItem } from '../components/todo-item/todo-item';
+
+@Component({
+  selector: 'app-todos',
+  imports: [TodoItem],
+  templateUrl: './todos.html',
+  styleUrl: './todos.scss',
+})
+export class Todos implements OnInit {
+  todoService = inject(TodoService);
+  todoItems = signal<Array<Todo>>([]);
+
+  ngOnInit(): void {
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+  }
+
+  updateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if (todo.id == todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+    });
+  }
+}
